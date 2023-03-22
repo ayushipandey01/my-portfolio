9ab2dc45cec66e22184e7fd4301c6d4b2b077e13
Ayushi Pandey <ayushipandey75@gmail.com>
2023-03-22 23:58:16 +0530

    portfolio website created

---
+++ script.js
@@ -0,0 +1,15 @@
+//Typing animations
+
+var typed = new Typed(".typing" , {
+    strings : ["Coder" , "Designer" , "Blogger" , "Developer"],
+    typeSpeed : 100,
+    backSpeed: 60,
+    loop: true,
+});
+
+var typed = new Typed(".typing-2" , {
+    strings : ["Coder" , "Designer" , "Blogger" , "Developer"],
+    typeSpeed : 99,
+    backSpeed: 58,
+    loop: true,
+});
