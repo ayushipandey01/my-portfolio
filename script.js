7b1487e3bb70c93691777161e7d90ec30929009e
Ayushi Pandey <ayushipandey75@gmail.com>
2023-04-09 00:53:23 +0530

    made the portfolio responsive using media queries

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 //Typing animations
 
 var typed = new Typed(".typing" , {
-    strings : ["Coder" , "Designer" , "Blogger" , "Developer"],
+    strings : ["Developer" , "Coder" , "Designer" , "Blogger"],
     typeSpeed : 100,
     backSpeed: 60,
     loop: true,
 });
 
 var typed = new Typed(".typing-2" , {
-    strings : ["Coder" , "Designer" , "Blogger" , "Developer"],
+    strings : ["Developer" , "Coder" , "Designer" , "Blogger"],
     typeSpeed : 99,
     backSpeed: 58,
     loop: true,
